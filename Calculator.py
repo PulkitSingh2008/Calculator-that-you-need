d55f20dfec83770c03c46bb8db4f47e1408daa8a
Pulkit0076 <71369943+Pulkit0076@users.noreply.github.com>
2020-10-07 15:56:14 +0530

    Calculator

--- Calculator.py
+++ Calculator.py
@@ -1,1 +1,27 @@
-print("None")
+print("CALCULATOR")
+#The first number given by the user
+num1=float(input("Please enter your first number: "))
+print(num1)
+#The second number given by the user
+num2=float(input("Please enter your second number: "))
+print(num2)
+#This is the operator given by the user
+operator=input("Please enter your arithmetic operator: ")
+
+print("Solution: ")
+print(num1)
+print(num2)
+print(operator)
+print("--------------")
+
+# if statements to perform calculations
+if operator == "+":
+	print(num1+num2)
+elif operator =="-":
+	print(num1-num2)
+elif operator=="*":
+	print(num1*num2)
+elif operator=="/":
+	print(num1/num2)
+else :
+	print("Answer not found!!")
